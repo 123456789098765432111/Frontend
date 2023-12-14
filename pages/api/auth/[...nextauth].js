b5670039426d8b6864a8d06c012e14e28680906a
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2023-12-14 22:33:51 +0500

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -16,18 +16,9 @@ export const authOptions = {
   callbacks: {
     session: ({session,token,user}) => {
         return session;
-   
     },
   },
 };
 
 export default NextAuth(authOptions);
 
-// export async function isAdminRequest(req,res) {
-//   const session = await getServerSession(req,res,authOptions);
-//   if (!adminEmails.includes(session?.user?.email)) {
-//     res.status(401);
-//     res.end();
-//     throw 'not an admin';
-//   }
-// }
